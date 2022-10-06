6e6833c14c8087ba8c2657ef89f70bb1faa23d33
Netetra <netetra071507@gmail.com>
2022-10-06 22:34:55 +0900

    first commit

---
+++ airootfs/root/chroot_install.sh
@@ -0,0 +1,37 @@
+pacman -S
+    pipewire-jack \
+    noto-fonts \
+    noto-fonts-cjk \
+    noto-fonts-emoji \
+    ttf-font-awesome \
+    fcitx5-mozc \
+    fcitx5-configtool \
+    gcc \
+    python \
+    rustup \
+    firefox \
+    foot \
+    git \
+    xorg-server-xwayland \
+    qt5-wayland \
+    sway \
+    swaybg \
+    polkit \
+    waybar \
+    wofi \
+    man-db \
+    vim \
+    nano \
+    zsh \
+    grml-zsh-config \
+    zsh-autosuggestions \
+    zsh-completions \
+    zsh-syntax-highlighting \
+    networkmanager \
+    wpa_supplicant \
+    dialog \
+    iw \
+    dhcpcd \
+    netctl \
+    wayvnc \
+    base-devel
---
+++ airootfs/root/install.sh
@@ -0,0 +1,1 @@
+pacstrap /mnt base linux-mbp linux-mbp-headers apple-bce-dkms-git apple-ibridge-dkms-git
